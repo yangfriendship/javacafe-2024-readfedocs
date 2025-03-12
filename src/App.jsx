de98c430356bce2f58c5978caeb713306db5f596
yangfriendship <yangfriendship@naver.com>
2025-03-12 23:43:51 +0900

    Board 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import './styles.css';
 
-export function Square() {
-    return <button className="square">X</button>;
+export function Board() {
+    return (
+        <>
+            <div className="board-row">
+                <button className="square">1</button>
+                <button className="square">2</button>
+                <button className="square">3</button>
+            </div>
+            <div className="board-row">
+                <button className="square">4</button>
+                <button className="square">5</button>
+                <button className="square">6</button>
+            </div>
+            <div className="board-row">
+                <button className="square">7</button>
+                <button className="square">8</button>
+                <button className="square">9</button>
+            </div>
+        </>
+    )
 }
 
 function App() {
 
     return (
         <>
-            <Square/>
+            <Board/>
         </>
     )
 }
