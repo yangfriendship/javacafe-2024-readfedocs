14b79b367793d686b73dd871a3544cbc0634b409
yangfriendship <yangfriendship@naver.com>
2025-03-16 12:48:46 +0900

    과거 움직임 보여주기
    - key App.jsx:109 Each child in a list should have a unique "key" prop. 에러 발생

--- src/App.jsx
+++ src/App.jsx
@@ -81,6 +81,24 @@ function Game() {
         setXIsNext(!xIsNext);
     }
 
+    function jumpTo(nextMove) {
+        // TODO
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+            description = 'Go to move #' + move;
+        } else {
+            description = 'Go to game start';
+        }
+        return (
+            <li>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
+    });
+
 
     return (
         <div className="game">
@@ -88,7 +106,7 @@ function Game() {
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
             </div>
             <div className="game-info">
-                <ol>{/*TODO*/}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
     );
