8d5b103b419b2bc1feba78123a992a23eafaa49a
yangfriendship <yangfriendship@naver.com>
2025-03-16 12:20:39 +0900

    state 끌어올리기

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 import './styles.css';
 import {useState} from "react";
 
-function Square() {
+function Square({value, onSquareClick}) {
 
-    const [value, setValue] = useState(null);
-
-    function handleClick() {
-        setValue('X');
-    }
-
-    return <button className="square" onClick={handleClick}>{value}</button>;
+    return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 export function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick(index) {
+        const nextSquares = squares.slice();
+        nextSquares[index] = "X";
+        setSquares(nextSquares);
+    }
+
     return (
         <>
             <div className="board-row">
-                <Square/>
-                <Square/>
-                <Square/>
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
             </div>
             <div className="board-row">
-                <Square/>
-                <Square/>
-                <Square/>
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
             </div>
             <div className="board-row">
-                <Square/>
-                <Square/>
-                <Square/>
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
             </div>
         </>
     )
